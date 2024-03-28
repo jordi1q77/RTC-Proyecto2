4ec1598ebfda54e01cd66788e07a6f0868841ea3
jordi1q77 <jordi1q77@gmail.com>
2024-03-28 06:50:14 +0100

    v.2.0

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import './style.css';
+import './tabla.css';
 /*import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 */
 import data from './elements.json';
-const categories = ["All"];
 let categoria = "All";
 let atomicMass = 999999.9999;
 /*const renderer = new THREE.WebGLRenderer({antialias:true});
@@ -59,87 +59,158 @@ animate();
 */
 
 
-const annadirElemento = (ul,element) => {
-  const {name, atomic_mass, symbol} = element;
-  const {title:title_img, url: url_img} = element.image;
-  const li = document.createElement("li");
-  li.classList.add ("element");
-  const title = document.createElement("h2");
-  title.textContent = name;
-  title.classList.add("element-name");
-  
-  const figure = document.createElement("figure");
-  const div = document.createElement("div");
-  div.classList.add("image-element");
-  const img = document.createElement("img");
-  img.src = url_img;
-  img.alt = title_img;
-
-  li.appendChild(title);
-  div.appendChild(img);
-
-  const span1 = document.createElement("span");
-  span1.textContent = symbol;
-  span1.classList.add("symbol");
-  div.appendChild(span1);
-  const span2 = document.createElement("span");
-  span2.classList.add("atomic-mass");
-  span2.textContent = atomic_mass;
-  div.appendChild(span2);
-
-  figure.appendChild(div);
-    
-    li.append(figure);
-  ul.append(li);
-}
-const pintarAll = () => {
-  const elements = data.elements;
-
-  const ul = document.querySelector(".elements");
+const pintarTabla= (elements) =>{
+  const ul = document.querySelector(".tabla");
+  ul.innerHTML = "";
   for (const element of elements) {
-  //vemos cuantas categorias exiten
-    if (!categories.includes(element.category)){
-      categories.push(element.category);
-    }
-  //pintamos
-   annadirElemento(ul,element);
+
+    const { symbol, cpk_hex, xpos, ypos} = element;
+    const xposNext = xpos + 1;
+    const yposNext = ypos + 1;
+    const li = document.createElement("li");
+
+    li.classList.add ("tabla-element");
+    li.textContent = symbol;
+    li.id = symbol;
+    li.style.gridColumnStart = xpos;
+    li.style.gridRowStart = ypos;
+    li.style.backgroundColor = cpk_hex;///TODO no funciona, revisar
+    li.style.borderColor = "#".concat(cpk_hex);
+    ul.append(li);
   }
 }
-const cargarFiltros = () => {
+const seleccionarElementos = (elements) => {
+  
+  for (const element of elements) {
+
+    const { symbol, cpk_hex, xpos, ypos} = element;
+    const htmlElement = document.getElementById(symbol);
+    htmlElement.classList.add("tabla-selected");
+  }
+}
+
+const pintarElementos= (elements) =>{
+  const ul = document.querySelector(".elements");
+  ul.innerHTML = "";
+  for (const element of elements) {
+
+    const {name, atomic_mass, symbol, cpk_hex} = element;
+    const {title:title_img, url: url_img} = element.image;
+    
+    const li = document.createElement("li");
+    const title = document.createElement("h2");
+    const figure = document.createElement("figure");
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+
+    li.classList.add ("element");
+    title.textContent = name;
+    title.classList.add("element-name");
+    
+    div.classList.add("image-element");
+
+    img.src = url_img;
+    img.alt = title_img;
+    span1.textContent = symbol;
+    span1.classList.add("symbol");
+    span2.classList.add("atomic-mass");
+    span2.textContent = atomic_mass;
+
+    span2.style.backgroundColor = cpk_hex;///TODO no funciona, revisar
+
+    li.appendChild(title);
+    div.appendChild(img);
+    div.appendChild(span1);
+    div.appendChild(span2);
+    figure.appendChild(div);  
+    li.append(figure);
+    ul.append(li);
+  }
+}
+const cargarCabeceraFiltro = () =>{
   const filter = document.querySelector(".filter");
   const filterTitle = document.createElement("H1");
   filterTitle.textContent = "Periodic table";
   filter.append(filterTitle);
+}
+const cargarFiltroCategoria = () => {
+  const filter = document.querySelector(".filter");
   const litCategory = document.createElement("label");
+  const selCategory = document.createElement("select");
+
   litCategory.textContent = "Categories";
   litCategory.for = "category";
-  filter.append(litCategory);
-  const selCategory = document.createElement("select");
   selCategory.classList.add("inputFilter");
   selCategory.id = "category";
   selCategory.addEventListener("change",filtrar);
+  
+  const totalCategories = data.elements.map(element => element.category);
+  const categories = [...new Set(totalCategories)];//Quitamos duplicados
+  categories.unshift("All");//añadimos al principio la opción de todos
+
   for (const category of categories) {
     const option = document.createElement("option");
     option.text = category;
     option.name = category;
     selCategory.append(option);
   }
+
+  filter.append(litCategory);
   filter.append(selCategory);
+  
+}
 
 
+const cargarFiltroFase = () => {
+  const filter = document.querySelector(".filter");
+  const litPhase = document.createElement("label");
+  const selPhase = document.createElement("select");
+
+  litPhase.textContent = "Phase";
+  litPhase.for = "phase";
+  selPhase.classList.add("inputFilter");
+  selPhase.id = "phase";
+  selPhase.addEventListener("change",filtrar);
+  
+  const totalPhases = data.elements.map(element => element.phase);
+  const phases = [...new Set(totalPhases)];//Quitamos duplicados
+  phases.unshift("All");//añadimos al principio la opción de todos
+
+  for (const phase of phases) {
+    const option = document.createElement("option");
+    option.text = phase;
+    option.name = phase;
+    selPhase.append(option);
+  }
+
+  filter.append(litPhase);
+  filter.append(selPhase);
+  
+}
+
+const cargarFiltroAtomicMass = () => {
+
+  const filter = document.querySelector(".filter");
   const litAM = document.createElement("label");
+  const inputAM = document.createElement("input");
+
   litAM.textContent = "Maximum atomic mass ";
   litAM.for = "atomicMass";
-  filter.append(litAM);
-  const inputAM = document.createElement("input");
   inputAM.type = "Number";
   inputAM.id = "inputAM";
   inputAM.addEventListener("keyup",filtrar);
   inputAM.classList.add("inputFilter");
-  filter.append(inputAM);
 
+  filter.append(litAM);
+  filter.append(inputAM);
+}
+
+const cargarBotonLimpiar = () => {
+  const filter = document.querySelector(".filter");
   const btLimpiarFiltros = document.createElement("button");
-  btLimpiarFiltros.textContent = "Limpiar Filtros";
+  btLimpiarFiltros.textContent = "Reset";
   btLimpiarFiltros.addEventListener("click",limpiarFiltros);
   btLimpiarFiltros.classList.add("inputFilter");
   filter.append(btLimpiarFiltros);
@@ -148,47 +219,55 @@ const cargarFiltros = () => {
 const filtrar = (event) => {
   const selCategory = document.querySelector("#category");
   const valCategory = selCategory.value;
+  const searchByCategory = valCategory != "All";
+
   const inpAM = document.querySelector("#inputAM");
   const valAM = Number(inpAM.value);
-  const ul = document.querySelector(".elements");
-  const searchByCategory = valCategory != "All";
   const searchByAM = valAM > 0;
-  const elements = data.elements;
-  ul.innerHTML = "";
-  //console.log("AM: " + searchByAM + "Category: " + searchByCategory);
-  if (!searchByAM && !searchByCategory){
-    pintarAll();
-  }else if (searchByAM && searchByCategory){
-    elements.forEach((element) => {
-      //console.log(element.atomic_mass);
-      if ((valCategory === element.category)
-      && (valAM >= Number(element.atomic_mass))) {
-        annadirElemento(ul,element);
-      } 
-    })
-  }else if (searchByAM) {
-    elements.forEach((element) => {
-      if (valAM >= Number(element.atomic_mass)){
-        annadirElemento(ul,element);
-      }});
-  }else{
-    elements.forEach((element) => {
-      if (valCategory === element.category){
-        annadirElemento(ul,element);
-      }});
+
+  const selPhase = document.querySelector("#phase");
+  const valPhase= selPhase.value;
+  const searchByPhase = valPhase != "All";
+
+
+  let filteredElements = data.elements;
+
+  if (searchByAM){
+    filteredElements = filteredElements.filter(element => valAM >= Number(element.atomic_mass));
   }
+  if (searchByCategory){
+    filteredElements = filteredElements.filter(element => valCategory === element.category);
+  }
+  if (searchByPhase){
+    filteredElements = filteredElements.filter(element => valPhase === element.phase);
+  }
+  pintarTabla(data.elements);
+
+  pintarElementos(filteredElements);
+  seleccionarElementos(filteredElements);
 }
 
 
 const limpiarFiltros = (event) => {
   const categorias = document.querySelector("#category");
   const massAtomic = document.querySelector("#inputAM");
+  const phases = document.querySelector("#phase");
+
   categorias.value = "All";
   massAtomic.value = 0;
-  pintarAll();
+  phases.value = "All";
+  
+  pintarElementos(data.elements);
 }
 
 
 // onLoad
-pintarAll();
-cargarFiltros();
+cargarCabeceraFiltro();
+pintarElementos(data.elements);
+cargarFiltroCategoria();
+cargarFiltroAtomicMass();
+cargarFiltroFase();
+cargarBotonLimpiar();
+pintarTabla(data.elements);
+const symbol = document.querySelector(".symbol");
+symbol.style.backgroundColor = "white";
